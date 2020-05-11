8f61f15981dcb53a5031fce3d4c14b7e44c63da7
cjs220 <41757818+cjs220@users.noreply.github.com>
2020-05-11 19:06:07 +0100

    Began using sklearn wrapper

--- examples/mixtures.py
+++ examples/mixtures.py
@@ -28,103 +28,112 @@ def triple_mixture(gamma):
     )
     return dist
 
+def main():
+    # Build dataset
+    theta_0 = 0.05
+    theta_1 = 0.00
+    n_samples_per_theta = int(1e5)
 
-# Build dataset
-theta_0 = 0.05
-theta_1 = 0.00
-n_samples_per_theta = int(1e5)
+    ds = RatioDataset(
+        n_samples_per_theta=n_samples_per_theta,
+        simulator_func=triple_mixture,
+        theta_0_dist=theta_0,
+        theta_1_dist=theta_1
+    )
 
-ds = RatioDataset(
-    n_samples_per_theta=n_samples_per_theta,
-    simulator_func=triple_mixture,
-    theta_0_dist=theta_0,
-    theta_1_dist=theta_1
-)
+    # hyperparams
+    epochs = 50
+    patience = 5
+    lr = 1e-3
+    validation_split = 0.1
+    n_hidden = (10, 10)
+    num_samples = int(validation_split * 2 * n_samples_per_theta)
 
-# hyperparams
-epochs = 10
-patience = 2
-lr = 1e-3
-validation_split = 0.1
-
-n_hidden = (10, 10)
-activations = (
-    'tanh',
-    'relu'
-)
-
-callbacks = [
-    tf.keras.callbacks.EarlyStopping(restore_best_weights=True, patience=patience),
-    tf.keras.callbacks.TensorBoard()
-]
-
-x_dim = 1
-num_samples = int(validation_split * 2 * n_samples_per_theta)
-
-# Choose model types
-model_types = [
-    ('Regular', FrequentistUnparameterisedRatioModel),
-    ('Bayesian', BayesianUnparameterisedRatioModel)
-]
-
-# Evaluate over [-5, 5]
-x = np.linspace(-5, 5, int(1e4))
-df = pd.DataFrame(index=x)
-models = {}
-
-for (name, model_cls), activation in zip(model_types, activations):
-    # fit model
-    clf = model_cls(x_dim=1,
-                    num_samples=num_samples,
-                    activation=activation,
-                    n_hidden=n_hidden)
-    clf.compile(
+    compile_kwargs = dict(
         optimizer=tf.keras.optimizers.Adam(learning_rate=lr),
         loss=tf.keras.losses.BinaryCrossentropy(),
         metrics=['accuracy']
     )
-    clf.fit_dataset(ds,
-                    epochs=epochs,
-                    validation_split=validation_split,
-                    callbacks=callbacks,
-                    verbose=2)
-    models[name] = clf
 
-    # predict over x
-    y_pred = clf.predict(x, theta_0, theta_1)
-    lr_estimate = clf.predict_likelihood_ratio(x, theta_0, theta_1)
-    nllr = -np.log(lr_estimate)
-    df[f'y_pred ({name})'] = y_pred.squeeze()
-    df[f'NLLR ({name})'] = nllr.squeeze()
+    fit_kwargs = dict(
+        epochs=epochs,
+        validation_split=validation_split,
+        callbacks=[
+            tf.keras.callbacks.EarlyStopping(restore_best_weights=True, patience=patience),
+            tf.keras.callbacks.TensorBoard()
+        ],
+        verbose=2
+    )
 
-# Find ideal classifier probabilities, and true likelihood ratio
-y_pred_ideal = ideal_classifier_probs(x, triple_mixture, theta_0, theta_1)
-df['y_pred (Ideal)'] = y_pred_ideal
-df['NLLR (True)'] = negative_log_likelihood_ratio(x, triple_mixture, theta_0, theta_1)
 
-# Plot results
-f, axarr = plt.subplots(2, sharex=True)
+    # Choose model types
+    model_types = [
+        ('Regular', FrequentistUnparameterisedRatioModel, 'tanh'),
+        ('Bayesian', BayesianUnparameterisedRatioModel, 'relu')
+    ]
 
-for i, variable in enumerate(['y_pred', 'NLLR']):
-    cols = list(filter(lambda x: variable in x, df.columns))
-    for col in cols:
-        alpha = 0.5 if 'Bayesian' in col else 1
-        df[col].plot(ax=axarr[i], alpha=alpha)
-    axarr[i].legend()
-    plt.xlim([-1, 3])
+    # Evaluate over [-5, 5]
+    x = np.linspace(-5, 5, int(1e4))
+    df = pd.DataFrame(index=x)
+    models = {}
 
-# Look at calibration
-f, axarr = plt.subplots(2, sharex=True)
-for y in (0, 1):
-    filter_ds = ds.filter(ds.y == y)
-    df_calib = pd.DataFrame()
-    for name, clf in models.items():
-        pred = clf.predict_dataset(filter_ds)
-        col_name = f'{name} (y={y})'
-        df_calib[col_name] = pred.squeeze()
-        df_calib[col_name].plot.kde(ax=axarr[y], label=col_name)
-        axarr[y].legend()
+    for name, model_cls, activation in model_types:
+        for calibrated in (True, False):
+            calibration = 'isotonic' if calibrated else None
+
+            # fit model
+            clf = model_cls(x_dim=1,
+                            theta_dim=1,
+                            num_samples=num_samples,
+                            activation=activation,
+                            n_hidden=n_hidden,
+                            calibration=calibration,
+                            compile_kwargs=compile_kwargs,
+                            fit_kwargs=fit_kwargs)
+            clf.fit_dataset(ds)
+            models[name] = clf
+
+            # predict over x
+            y_pred = clf.predict_proba(x, theta_0, theta_1)[:, 1]
+            lr_estimate = clf.predict_likelihood_ratio(x, theta_0, theta_1)
+            nllr = -np.log(lr_estimate)
+            calibration_label = 'Calibrated' if calibrated else 'Uncalibrated'
+            label = f'({name}, {calibration_label})'
+            df[f'y_pred {label}'] = y_pred.squeeze()
+            df[f'NLLR {label}'] = nllr.squeeze()
+
+    # Find ideal classifier probabilities, and true likelihood ratio
+    y_pred_ideal = ideal_classifier_probs(x, triple_mixture, theta_0, theta_1)
+    df['y_pred (Ideal)'] = y_pred_ideal
+    df['NLLR (True)'] = negative_log_likelihood_ratio(x, triple_mixture, theta_0, theta_1)
+
+    # Plot results
+    f, axarr = plt.subplots(2, sharex=True)
+
+    for i, variable in enumerate(['y_pred', 'NLLR']):
+        cols = list(filter(lambda x: variable in x, df.columns))
+        for col in cols:
+            alpha = 0.5 if 'Bayesian' in col else 1
+            df[col].plot(ax=axarr[i], alpha=alpha)
+        axarr[i].legend()
+        plt.xlim([-1, 3])
+
+    # Look at calibration
+    f, axarr = plt.subplots(2, sharex=True)
+    for y in (0, 1):
+        filter_ds = ds.filter(ds.y == y)
+        df_calib = pd.DataFrame()
+        for name, clf in models.items():
+            pred = clf.predict_proba_dataset(filter_ds)[:, 1]
+            col_name = f'{name} (y={y})'
+            df_calib[col_name] = pred.squeeze()
+            df_calib[col_name].plot.kde(ax=axarr[y], label=col_name)
+            axarr[y].legend()
     plt.legend()
     plt.xlim([0.4, 0.6])
 
-plt.show()
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
--- model.py
+++ model.py
@@ -1,75 +1,16 @@
 import numpy as np
 import tensorflow as tf
 import tensorflow_probability as tfp
+from sklearn.calibration import CalibratedClassifierCV
+from tensorflow_core.python.keras import Sequential
+from tensorflow_core.python.keras.wrappers.scikit_learn import KerasClassifier
 
 from dataset import RatioDataset
 
 tfd = tfp.distributions
 
 
-class RatioModel(tf.keras.Model):
-
-    def __init__(self,
-                 input_dim,
-                 num_samples,
-                 n_hidden=(10, 10),
-                 activation='relu',
-                 output_activation='sigmoid'):
-        super().__init__()
-        self.input_dim = input_dim
-        self.num_samples = num_samples
-        self.dense_layers = self.build_layers(n_hidden=n_hidden,
-                                              activation=activation,
-                                              output_activation=output_activation)
-
-    def __call__(self, inputs, training=False, **kwargs):
-        for layer in self.dense_layers:
-            inputs = layer(inputs)
-        return inputs
-
-    def fit_dataset(self, dataset: RatioDataset, **kwargs):
-        model_input = self.build_input(dataset.x, dataset.theta_1, dataset.theta_0)
-        self.fit(x=model_input, y=dataset.y, **kwargs)
-
-    def predict(self, x, theta_0, theta_1, **kwargs):
-        model_input = self.build_input(x, theta_0, theta_1)
-        return super().predict(model_input, **kwargs)
-
-    def predict_dataset(self, ds, **kwargs):
-        return self.predict(ds.x, ds.theta_0, ds.theta_1, **kwargs)
-
-    def predict_likelihood_ratio(self, x, theta_0, theta_1, **kwargs):
-        y_pred = self.predict(x, theta_0, theta_1, **kwargs)
-        likelihood_ratio = y_pred / (1 - y_pred)
-        return likelihood_ratio
-
-    def predict_likelihood_ratio_dataset(self, ds, **kwargs):
-        return self.predict_likelihood_ratio(ds.x, ds.theta, ds.theta_1, **kwargs)
-
-    def build_input(self, x, theta_1, theta_0):
-        raise NotImplementedError
-
-    def build_layers(self, n_hidden=(10, 10), activation='relu', output_activation='sigmoid'):
-        layers = [self.dense_layer(units, activation) for units in n_hidden]
-        layers.append(self.dense_layer(1, output_activation))
-        return layers
-
-
-class UnparameterisedRatioModel(RatioModel):
-
-    def __init__(self, x_dim, num_samples, n_hidden=(10, 10), activation='relu', output_activation='sigmoid'):
-        input_dim = x_dim
-        super().__init__(input_dim=input_dim,
-                         num_samples=num_samples,
-                         n_hidden=n_hidden,
-                         activation=activation,
-                         output_activation=output_activation)
-
-    def build_input(self, x, theta_1, theta_0):
-        return x
-
-
-class SingleParameterisedRatioModel(RatioModel):
+class RatioModel:
 
     def __init__(self,
                  x_dim,
@@ -77,13 +18,99 @@ class SingleParameterisedRatioModel(RatioModel):
                  num_samples,
                  n_hidden=(10, 10),
                  activation='relu',
-                 output_activation='sigmoid'):
-        input_dim = x_dim + theta_dim
-        super().__init__(input_dim=input_dim,
-                         num_samples=num_samples,
-                         n_hidden=n_hidden,
-                         activation=activation,
-                         output_activation=output_activation)
+                 output_activation='sigmoid',
+                 calibration=None,
+                 compile_kwargs=None,
+                 fit_kwargs=None):
+        self.x_dim = x_dim
+        self.theta_dim = theta_dim
+        self.num_samples = num_samples
+        self.n_hidden = n_hidden
+        self.activation = activation
+        self.output_activation = output_activation
+        self.calibration = calibration
+        self.compile_kwargs = compile_kwargs if compile_kwargs is not None else dict()
+        self.fit_kwargs = fit_kwargs if fit_kwargs is not None else dict()
+        self.estimator = KerasClassifier(build_fn=self._build_fn, **fit_kwargs)
+        if self.calibrated:
+            assert calibration in ('sigmoid', 'isotonic')
+            self.estimator = CalibratedClassifierCV(base_estimator=self.estimator)
+
+    def _build_fn(self):
+        model = Sequential()
+        dense_layers = self._build_layers(n_hidden=self.n_hidden,
+                                          activation=self.activation,
+                                          output_activation=self.output_activation)
+        for layer in dense_layers:
+            model.add(layer)
+        model.compile(**self.compile_kwargs)
+        return model
+
+    def _build_layers(self, n_hidden=(10, 10), activation='relu', output_activation='sigmoid'):
+        layers = [self.dense_layer(units, activation) for units in n_hidden]
+        layers.append(self.dense_layer(1, output_activation))
+        return layers
+
+    @property
+    def calibrated(self):
+        return self.calibration is not None
+
+    def fit(self, x, theta_0, theta_1, y):
+        model_input = self.build_input(x, theta_0, theta_1)
+        self.estimator.fit(model_input, y)
+        return self
+
+    def fit_dataset(self, dataset: RatioDataset):
+        self.fit(dataset.x, dataset.theta_0, dataset.theta_1, dataset.y)
+        return self
+
+    def predict(self, x, theta_0, theta_1):
+        model_input = self.build_input(x, theta_0, theta_1)
+        return self.estimator.predict(model_input)
+
+    def predict_proba(self, x, theta_0, theta_1):
+        model_input = self.build_input(x, theta_0, theta_1)
+        return self.estimator.predict_proba(model_input)
+
+    def predict_likelihood_ratio(self, x, theta_0, theta_1):
+        y_pred = self.predict_proba(x, theta_0, theta_1)[:, 1]
+        likelihood_ratio = y_pred / (1 - y_pred)
+        return likelihood_ratio
+
+    def predict_dataset(self, dataset):
+        return self.predict(dataset.x, dataset.theta_0, dataset.theta_1)
+
+    def predict_proba_dataset(self, dataset):
+        return self.predict_proba(dataset.x, dataset.theta_0, dataset.theta_1)
+
+    def predict_likelihood_ratio_dataset(self, dataset):
+        return self.predict_likelihood_ratio(dataset.x, dataset.theta, dataset.theta_1)
+
+    @property
+    def input_dim(self):
+        raise NotImplementedError
+
+    def build_input(self, x, theta_1, theta_0):
+        raise NotImplementedError
+
+
+class UnparameterisedRatioModel(RatioModel):
+
+    @property
+    def input_dim(self):
+        return self.x_dim
+
+    def build_input(self, x, theta_1, theta_0):
+        if len(x.shape) == 1:
+            x = x.reshape(-1, 1)
+        return x
+
+
+class SingleParameterisedRatioModel(RatioModel):
+
+    @property
+    def input_dim(self):
+        return self.x_dim + self.theta_dim
 
     def build_input(self, x, theta_1, theta_0):
         # TODO
@@ -92,19 +119,9 @@ class SingleParameterisedRatioModel(RatioModel):
 
 class DoubleParameterisedRatioModel(RatioModel):
 
-    def __init__(self,
-                 x_dim,
-                 theta_dim,
-                 num_samples,
-                 n_hidden=(10, 10),
-                 activation='relu',
-                 output_activation='sigmoid'):
-        input_dim = x_dim + 2 * theta_dim
-        super().__init__(input_dim=input_dim,
-                         num_samples=num_samples,
-                         n_hidden=n_hidden,
-                         activation=activation,
-                         output_activation=output_activation)
+    @property
+    def input_dim(self):
+        return self.x_dim + 2*self.theta_dim
 
     def build_input(self, x, theta_1, theta_0):
         # TODO
