ad53f84c4395cd87f7e78843cf5a2ab30b8cff2b
cjs220 <41757818+cjs220@users.noreply.github.com>
2020-05-17 12:04:15 +0100

    Contour plot + construct dataset

--- active_learning_ratio_estimation/dataset.py
+++ active_learning_ratio_estimation/dataset.py
@@ -20,12 +20,24 @@ class DataSet:
     def __add__(self, other):
         return self.__class__(self.samples + other.samples)
 
-    def __getattr__(self, item):
-        if item in self.dataframe.columns:
-            return getattr(self.dataframe, item).values
-
     def __len__(self):
         return len(self.samples)
+
+    @property
+    def x(self):
+        return np.array([sample['x'] for sample in self.samples])
+
+    @property
+    def y(self):
+        return np.array([sample['y'] for sample in self.samples])
+
+    @property
+    def theta_0(self):
+        return np.array([sample['theta_0'] for sample in self.samples])
+
+    @property
+    def theta_1(self):
+        return np.array([sample['theta_1'] for sample in self.samples])
 
     def shuffle(self):
         shuffle(self.samples)
@@ -43,6 +55,7 @@ class DataSet:
 
     @classmethod
     def from_csv(cls, path_or_buf):
+        # TODO: fix for multidim x
         df = pd.read_csv(path_or_buf)
         return cls.from_dataframe(df)
 
@@ -59,18 +72,19 @@ class RatioDataset(DataSet):
                  simulator_func: Callable,
                  theta_0_dist: Union[tfd.Distribution, Number, np.array],
                  theta_1_dist: Union[tfd.Distribution, Number, np.array],
-                 n_thetas: int = 1):
+                 n_thetas: int = None):
 
-        theta_0 = self._choose_theta(theta_0_dist, n_thetas)
-        theta_1 = self._choose_theta(theta_1_dist, n_thetas)
+        theta_0s = self._choose_theta(theta_0_dist, n_thetas)
+        theta_1s = self._choose_theta(theta_1_dist, n_thetas)
 
         samples = []
-        for theta_0, theta_1 in zip(theta_0, theta_1):
+        for theta_0, theta_1 in zip(theta_0s, theta_1s):
             for theta, y in [(theta_0, 0), (theta_1, 1)]:
                 x = self.simulate(simulator_func=simulator_func, theta=theta, n_samples_per_theta=n_samples_per_theta)
                 new_samples = [dict(x=x_i, theta_0=theta_0, theta_1=theta_1, y=y) for x_i in x]
                 samples += new_samples
         shuffle(samples)
+
         super().__init__(samples)
 
     @staticmethod
@@ -78,18 +92,17 @@ class RatioDataset(DataSet):
         if hasattr(dist, 'sample'):
             theta = dist.sample(n_samples).numpy()
         elif isinstance(dist, Number):
-                theta = np.array([dist] * n_samples)
+            # TODO: remove this
+            theta = np.array([dist] * n_samples)
         else:
-            if len(dist.shape) == 2:
-                # dist is an array of thetas
-                theta = dist
-            else:
-                assert len(dist.shape == 1)
-                theta = np.stack([dist for _ in range(n_samples)], 0)
+            # theta is an array of shape (num_thetas, -1)
+            assert len(dist.shape) == 2
+            assert n_samples is None
+            theta = dist
         return theta
 
     @staticmethod
     def simulate(simulator_func, theta, n_samples_per_theta):
-        simulator = simulator_func(theta)
+        simulator = simulator_func(*theta)
         x = simulator.sample(n_samples_per_theta).numpy()
         return x
