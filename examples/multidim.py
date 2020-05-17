ee81cc1531233f069ff89cfcf26c77d7f284e083
cjs220 <41757818+cjs220@users.noreply.github.com>
2020-05-17 12:04:02 +0100

    Update multidim.py

--- examples/multidim.py
+++ examples/multidim.py
@@ -6,6 +6,7 @@
 
 import sys
 from functools import partial
+from itertools import product
 
 from scipy.stats import chi2
 
@@ -82,51 +83,84 @@ X_true = p_true.sample(500)
 
 
 # find exact maximum likelihood
-# var_alpha = tf.Variable(tf.constant(0, dtype=tf.float32))
-# var_beta = tf.Variable(tf.constant(0, dtype=tf.float32))
-# p_var = MultiDimToyModel(var_alpha, var_beta)
-# optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
-# n_iter = int(1e3)
-#
-# for i in range(n_iter):
-#     loss_fn = lambda: -tf.keras.backend.sum(p_var.log_prob(X_true))
-#     optimizer.minimize(loss_fn, [var_alpha, var_beta])
-#
-# print(f'Exact MLE: alpha={var_alpha.numpy()}, beta={var_beta.numpy()}')
+var_alpha = tf.Variable(tf.constant(0, dtype=tf.float32))
+var_beta = tf.Variable(tf.constant(0, dtype=tf.float32))
+p_var = MultiDimToyModel(var_alpha, var_beta)
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
+n_iter = int(1e3)
+nll = tf.function(lambda: -tf.keras.backend.sum(p_var.log_prob(X_true)))
+
+for i in range(n_iter):
+    optimizer.minimize(nll, [var_alpha, var_beta])
+
+
+alpha_mle = var_alpha.numpy()
+beta_mle = var_beta.numpy()
+max_log_prob = p_var.log_prob(X_true)
+
+print(f'Exact MLE: alpha={alpha_mle}, beta={beta_mle}')
 
 
 # %%
 
 
-# Plot contours of exact negative log likelihood ratio
-p_1 = MultiDimToyModel(alpha=0, beta=0)  # define reference distribution p_1 to be defined at alpha=beta=0
+# Calculate contours of exact negative log likelihood ratio
+
+# @tf.function
+# def nllr_exact(alpha, beta, X):
+#     p_theta = MultiDimToyModel(alpha=alpha, beta=beta)
+#     return -tf.keras.backend.sum((p_theta.log_prob(X) - max_log_prob))
 
 
-@tf.function
-def nllr_exact(alpha, beta, X):
-    p_theta = MultiDimToyModel(alpha=alpha, beta=beta)
-    return -tf.keras.backend.sum((p_theta.log_prob(X) - p_1.log_prob(X)))
-
-
-n_grid = 100
-alpha_bounds = (0.85, 1.15)
-beta_bounds = (-1.5, 1.5)
+n_grid = 10
+alpha_bounds = (0.75, 1.25)
+beta_bounds = (-2, 0)
 alphas = np.linspace(alpha_bounds[0], alpha_bounds[1], n_grid).astype(np.float32)
 betas = np.linspace(beta_bounds[0], beta_bounds[1], n_grid).astype(np.float32)
-Alphas, Betas = np.meshgrid(alphas, betas)
+# Alphas, Betas = np.meshgrid(alphas, betas)
+#
+# exact_contours = np.zeros_like(Alphas)
+# for i in range(n_grid):
+#     for j in range(n_grid):
+#         alpha = tf.constant(Alphas[i, j])
+#         beta = tf.constant(Betas[i, j])
+#         nllr = nllr_exact(alpha, beta, X_true)
+#         exact_contours[i, j] = nllr
 
-exact_contours = np.zeros_like(Alphas)
-for i in range(n_grid):
-    for j in range(n_grid):
-        alpha = tf.constant(Alphas[i, j])
-        beta = tf.constant(Betas[i, j])
-        exact_contours[i, j] = nllr_exact(alpha, beta, X_true)
+
+#%%
 
 
-plt.figure()
-plt.contour(Alphas, Betas, exact_contours,
-            levels=[chi2.ppf(0.683, df=2),
-                    chi2.ppf(0.9545, df=2),
-                    chi2.ppf(0.9973, df=2)], colors=["w"])
-plt.contourf(Alphas, Betas, exact_contours, 50, vmin=0, vmax=30)
+# plot contours
+# plt.figure()
+# plt.contour(Alphas, Betas, exact_contours,
+#             levels=[chi2.ppf(0.683, df=2),
+#                     chi2.ppf(0.9545, df=2),
+#                     chi2.ppf(0.9973, df=2)], colors=["w"])
+# plt.contourf(Alphas, Betas, exact_contours, 50, vmin=0, vmax=30)
+# plt.plot([true_alpha], [true_beta],'ro', markersize=8, label='True')
+# plt.plot([alpha_mle], [beta_mle], 'go', markersize=8, label='MLE')
+# plt.xlabel(r"$\alpha$")
+# plt.ylabel(r"$\beta$")
+# plt.title(r'$-2\log \left[ L(\theta) / L(\theta _\mathrm{MLE}) \right]$')
+# plt.xlim(alpha_bounds)
+# plt.ylim(beta_bounds)
+# cb = plt.colorbar()
+# plt.legend(loc='upper center', fancybox=True, shadow=True)
+# plt.show()
+
+
+#%%
+
+
+# create dataset for fitting
+theta_1 = np.array(list(product(alphas, betas)))
+theta_0 = np.array([(alpha_mle, beta_mle) for _ in range(len(theta_1))])
+
+ds = RatioDataset(n_samples_per_theta=10,
+                  simulator_func=MultiDimToyModel,
+                  theta_0_dist=theta_0,
+                  theta_1_dist=theta_1)
+
+print(ds)
 pass
